ab614ad7bab73e6e0c52598d72efde815c08d50f
Alejandro Domínguez <alejandrodominguezc7@gmail.com>
2023-02-02 20:30:43 +0000

    Update config.php

--- views/config.php
+++ views/config.php
@@ -2,10 +2,10 @@
     // Configuramos nuestro enlace a la bd.
     return [
         'db' => [
-            'host' => 'localhost',
-            'user' => 'root',
-            'pass' => '',
-            'name' => 'gestionSalas',
+            'host' => $_ENV['DB_HOST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'name' => $_ENV['DB_DB'],
             'options' => [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
